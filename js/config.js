538c3a36483de6466abbb861b02a8bc050945ba0
Ajacko97 <45338619+Ajacko97@users.noreply.github.com>
2019-03-07 11:49:45 +0100

    Change the account for paypal

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ $(function() {
 	    // how simpleCart should checkout, see the checkout reference for more info
 	    checkout: {
 	        type: "PayPal" ,
-	        email: "jace0244@edu.easj.dk"
+	        email: "raluking@yahoo.com"
 	    },
 
 	    // set the currency, see the currency reference for more info
